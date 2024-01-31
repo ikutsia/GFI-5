684a65d43b2996e20a9dea77ac3e3d9d4b7f2245
irakli <ikutsia@gmail.com>
2024-01-31 13:10:17 +0400

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  // other webpack config options...
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+    ],
+  },
+};
